6f2f51d82a51a3f498002daa677eaea972e02047
Stefan Halvadzhiev <feneris360@gmail.com>
2024-03-12 20:42:17 +0200

    feature: add testing function for mean

--- testing_functions/operations_on_number_files.py
+++ testing_functions/operations_on_number_files.py
@@ -3,34 +3,42 @@ import numpy as np
 
 
 # MEDIAN
-def median_rows(df: pd.DataFrame):
+def median_rows(df: pd.DataFrame) -> None:
     pass
 
 
-def median_columns(df: pd.DataFrame):
+def median_columns(df: pd.DataFrame) -> None:
     pass
 
 
-def median_rows_improved(df: pd.DataFrame):
+def median_rows_improved(df: pd.DataFrame) -> None:
     pass
 
 
-def median_columns_improved(df: pd.DataFrame):
+def median_columns_improved(df: pd.DataFrame) -> None:
     pass
 
 
 # MEAN
-def mean_rows(df: pd.DataFrame):
-    pass
+def mean_rows(df: pd.DataFrame) -> pd.DataFrame:
+    row_means = df.mean(axis=1)
+    result_df = pd.DataFrame(row_means, columns=['Mean'])
+    return result_df
 
 
-def mean_columns(df: pd.DataFrame):
-    pass
+def mean_columns(df: pd.DataFrame) -> None:
+    row_means = df.mean(axis=0)
+    result_df = pd.DataFrame(row_means, columns=['Mean'])
+    return result_df
 
 
-def mean_rows_improved(df: pd.DataFrame):
-    pass
+def mean_rows_improved(df: pd.DataFrame) -> pd.DataFrame:
+    row_means = np.mean(df.to_numpy(), axis=1)
+    result_df = pd.DataFrame(row_means, columns=['Mean'])
+    return result_df
 
 
-def mean_columns_improved(df: pd.DataFrame):
-    pass
+def mean_columns_improved(df: pd.DataFrame) -> None:
+    row_means = np.mean(df.to_numpy(), axis=0)
+    result_df = pd.DataFrame(row_means, columns=['Mean'])
+    return result_df
