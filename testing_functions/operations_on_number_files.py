80c3521ba53b858da776b9424867e6d7781a7ad6
Stefan Halvadzhiev <feneris360@gmail.com>
2024-03-18 19:31:58 +0200

    feature: compare numerical operations with numpy and pandas

--- testing_functions/operations_on_number_files.py
+++ testing_functions/operations_on_number_files.py
@@ -3,42 +3,42 @@ import numpy as np
 
 
 # MEDIAN
-def median_rows(df: pd.DataFrame) -> None:
-    pass
+def median_rows(df: pd.DataFrame) -> pd.DataFrame:
+    row_medians = df.median(axis=1)
+    return pd.DataFrame(row_medians, columns=['Median'])
+    
 
+def median_columns(df: pd.DataFrame) -> pd.DataFrame:
+    col_medians = df.median(axis=0)
+    return pd.DataFrame(col_medians, columns=['Median'])
+    
 
-def median_columns(df: pd.DataFrame) -> None:
-    pass
+def median_rows_improved(df: pd.DataFrame) -> pd.DataFrame:
+    row_medians = np.median(df.to_numpy(), axis=1)
+    return pd.DataFrame(row_medians, columns=['Median'])
+    
 
-
-def median_rows_improved(df: pd.DataFrame) -> None:
-    pass
-
-
-def median_columns_improved(df: pd.DataFrame) -> None:
-    pass
-
+def median_columns_improved(df: pd.DataFrame) -> pd.DataFrame:
+    col_medians = np.median(df.to_numpy(), axis=0)
+    return pd.DataFrame(col_medians, columns=['Median'])
+    
 
 # MEAN
 def mean_rows(df: pd.DataFrame) -> pd.DataFrame:
     row_means = df.mean(axis=1)
-    result_df = pd.DataFrame(row_means, columns=['Mean'])
-    return result_df
-
+    return pd.DataFrame(row_means, columns=['Mean'])
+    
 
 def mean_columns(df: pd.DataFrame) -> None:
     row_means = df.mean(axis=0)
-    result_df = pd.DataFrame(row_means, columns=['Mean'])
-    return result_df
-
+    return pd.DataFrame(row_means, columns=['Mean'])
+    
 
 def mean_rows_improved(df: pd.DataFrame) -> pd.DataFrame:
     row_means = np.mean(df.to_numpy(), axis=1)
-    result_df = pd.DataFrame(row_means, columns=['Mean'])
-    return result_df
-
+    return pd.DataFrame(row_means, columns=['Mean'])
+    
 
 def mean_columns_improved(df: pd.DataFrame) -> None:
-    row_means = np.mean(df.to_numpy(), axis=0)
-    result_df = pd.DataFrame(row_means, columns=['Mean'])
-    return result_df
+    col_means = np.mean(df.to_numpy(), axis=0)
+    return pd.DataFrame(col_means, columns=['Mean'])
